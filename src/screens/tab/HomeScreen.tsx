a73be25084423dcbe031383a9cbf2831508f687f
marvelous-ikechi <marvelike7@gmail.com>
2025-03-15 12:23:57 +0100

    Add HomeScreen component with ScreenWrapper and View

---
+++ src/screens/tab/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import React, {FunctionComponent} from 'react';
+
+import ScreenWrapper from 'src/components/container/ScreenWrapper';
+import View from 'src/components/ui/View/View';
+
+const HomeScreen: FunctionComponent = () => {
+  return (
+    <ScreenWrapper>
+      <View />
+    </ScreenWrapper>
+  );
+};
+
+export default HomeScreen;
