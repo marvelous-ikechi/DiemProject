2ceaa812ac7f2fcfb8da2a51d5287e22cac04d12
marvelous-ikechi <marvelike7@gmail.com>
2025-03-15 18:08:42 +0100

    Add react-native-config and dotenv integration with type definitions

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,31 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          src: './src',
-          '@assets': './src/assets',
-          '@components': './src/components',
-          '@hooks': './src/hooks',
-          '@navigation': './src/navigation',
-          '@screens': './src/screens',
-          '@services': './src/services',
-          '@utils': './src/utils',
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            src: './src',
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@hooks': './src/hooks',
+            '@navigation': './src/navigation',
+            '@screens': './src/screens',
+            '@services': './src/services',
+            '@utils': './src/utils',
+          },
         },
-      },
+      ],
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'APP_ENV',
+          moduleName: '@env',
+          path: '.env',
+        },
+      ],
     ],
-  ],
+  };
 };
