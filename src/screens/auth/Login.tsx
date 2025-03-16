6b6cf5b57d237486fae595fde47c97e329bfa722
marvelous-ikechi <marvelike7@gmail.com>
2025-03-16 13:20:08 +0100

    Implement email and password input handling in Login component with loading states

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent} from 'react';
+import React, {FunctionComponent, useState} from 'react';
 
 import {AuthStackParamList} from '@src/navigation/types/AuthStackParamList';
 import GoogleIcon from 'src/assets/Icons/GoogleIcon';
@@ -14,8 +14,16 @@ import useSignIn from 'src/hooks/useSignIn';
 
 type Props = NativeStackScreenProps<AuthStackParamList, 'Login'>;
 
-const Login: FunctionComponent<Props> = ({navigation}) => {
-  const {googleSignIn} = useSignIn();
+const Login: FunctionComponent<Props> = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const {
+    googleSignIn,
+    signInOrSignUpWithEmailAndPassword,
+    isEmailAndPasswordSignInLoading,
+    isGoogleSignInLoading,
+  } = useSignIn();
 
   return (
     <ScreenWrapper>
@@ -26,15 +34,29 @@ const Login: FunctionComponent<Props> = ({navigation}) => {
         Welcome back
       </Text>
       <View style={styles.inputFieldsContainer}>
-        <TextInput label="Email Address" />
-        <TextInput label="Password" />
-        <SubmitButton text="Login" onPress={() => {}} textColor="secondary" />
+        <TextInput
+          label="Email Address"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          label="Password"
+          value={password}
+          onChangeText={setPassword}
+        />
+        <SubmitButton
+          text="Login"
+          onPress={() => signInOrSignUpWithEmailAndPassword(email, password)}
+          textColor="secondary"
+          isLoading={isEmailAndPasswordSignInLoading}
+        />
         <SubmitButton
           text="Sign up with google"
           onPress={googleSignIn}
           textColor="secondary"
           backgroundColor="secondary"
           leftIcon={<GoogleIcon style={styles.iconStyle} />}
+          isLoading={isGoogleSignInLoading}
         />
       </View>
     </ScreenWrapper>
