8dbc2871716c04f3c4907fb24ceb00507168eec3
marvelous-ikechi <marvelike7@gmail.com>
2025-03-16 18:24:41 +0100

    Implement conditional navigation based on user authentication state; integrate Zustand store to determine screen flow in RootNavigator

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -3,15 +3,20 @@ import BottomTab from './BottomNavigator';
 import {NavigationContainer} from '@react-navigation/native';
 import {RootStackParamList} from './types/RootStackParamList';
 import {createStackNavigator} from '@react-navigation/stack';
-
+import useStore from 'src/store/store';
 const Stack = createStackNavigator<RootStackParamList>();
 
 const RootNavigator = () => {
+  const {user} = useStore();
+  console.log('user', user);
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="AuthNavigator" component={AuthNavigator} />
-        <Stack.Screen name="BottomNavigator" component={BottomTab} />
+        {user === null ? (
+          <Stack.Screen name="AuthNavigator" component={AuthNavigator} />
+        ) : (
+          <Stack.Screen name="BottomNavigator" component={BottomTab} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
