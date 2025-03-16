772c9ec7ee16fa26e7f9fc1547feace59a55b5e0
marvelous-ikechi <marvelike7@gmail.com>
2025-03-16 21:09:05 +0100

    Add PokemonDetails component to display individual Pokémon information

---
+++ src/screens/home/PokemonDetails.tsx
@@ -0,0 +1,9 @@
+import React, {FunctionComponent} from 'react';
+
+import {View} from 'react-native';
+
+const PokemonDetails: FunctionComponent = () => {
+  return <View />;
+};
+
+export default PokemonDetails;
