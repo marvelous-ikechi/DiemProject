a7e4b3d02e7897173c5397c12f363a3aeafca9a7
marvelous-ikechi <marvelike7@gmail.com>
2025-03-16 21:08:31 +0100

    Integrate React Query for data fetching in App component; wrap application in QueryClientProvider and add axios dependency.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import './gesture-handler';
 import 'src/services/firebase';
 
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {StatusBar, View, useColorScheme} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
@@ -9,6 +10,7 @@ import RootNavigator from 'src/navigation/RootNavigator';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const queryClient = new QueryClient();
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -16,13 +18,15 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <RootNavigator />
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <View style={backgroundStyle}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <RootNavigator />
+      </View>
+    </QueryClientProvider>
   );
 }
 
