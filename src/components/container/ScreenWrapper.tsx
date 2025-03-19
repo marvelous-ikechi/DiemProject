9e55ef626b7fa1c4f47eeebb24ae82c7652e1ed5
marvelous-ikechi <marvelike7@gmail.com>
2025-03-19 13:52:02 +0100

    Enhance ScreenWrapper component by adding a back navigation button and updating props to support navigation functionality. Refactor PokemonDetails screen to utilize the new goBack prop for improved user experience.

--- src/components/container/ScreenWrapper.tsx
+++ src/components/container/ScreenWrapper.tsx
@@ -1,18 +1,36 @@
-import {Platform, SafeAreaView, StyleSheet} from 'react-native';
+import {
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import React, {FunctionComponent, ReactNode} from 'react';
 
+import {MoveLeft} from 'lucide-react-native';
 import View from '../ui/View/View';
 import {colors} from 'src/utils/colors';
+import {useNavigation} from '@react-navigation/native';
 
 interface Props {
   children: ReactNode;
   center?: boolean;
+  goBack?: boolean;
 }
 
-const ScreenWrapper: FunctionComponent<Props> = ({center, children}) => {
+const ScreenWrapper: FunctionComponent<Props> = ({
+  center,
+  children,
+  goBack,
+}) => {
+  const navigation = useNavigation();
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={[styles.container, center && styles.center]}>
+        {goBack && (
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <MoveLeft size={24} color={colors.blue} />
+          </TouchableOpacity>
+        )}
         {children}
       </View>
     </SafeAreaView>
