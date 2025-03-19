5400a530ed77738280f5e9c715ec4bde219bae57
marvelous-ikechi <marvelike7@gmail.com>
2025-03-19 16:41:28 +0100

    Refactor getPokemonImage function to use PokemonType for improved type safety and clarity in handling Pokémon data.

--- src/utils/pokemonUtils.ts
+++ src/utils/pokemonUtils.ts
@@ -1,4 +1,6 @@
-export const getPokemonImage = (pokemon: any) =>
+import {PokemonType} from 'src/types/appTypes';
+
+export const getPokemonImage = (pokemon: PokemonType) =>
   `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
     pokemon.url.split('/')[6]
   }.png`;
